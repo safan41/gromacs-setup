d6eea7aa96576fc04b299a3a905b3964d8a5cf22
Safwan Morshed <safan25641@gmail.com>
2025-07-10 13:26:08 -0500

    pls fix pls

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ bash <(curl -sSL conda.sh)
 eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
 conda init
 source ~/.bashrc
-version= "$(conda --version)"
+version= $(conda --version)
 
-if $version == conda*; then
+if [[ $version == conda* ]] then
     mkdir biobb && cd biobb
     curl -sS https://raw.githubusercontent.com/safan41/gromacs-setup/refs/heads/main/environment.yml
     conda env create -f environment.yml
