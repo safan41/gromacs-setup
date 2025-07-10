450b844c35e3dae65038981e4bc393ce95f1d464
Safwan Morshed <safan25641@gmail.com>
2025-07-10 13:19:22 -0500

    fix pls

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ bash <(curl -sSL conda.sh)
 eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
 conda init
 source ~/.bashrc
-version= conda --version
+version= "$(conda --version)"
 
 if $version == conda*; then
     mkdir biobb && cd biobb
