6aba96c2633176df836ee401667387f9715985dc
Safwan Morshed <safan25641@gmail.com>
2025-07-10 13:11:15 -0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+cd ~
+bash <(curl -sSL conda.sh)
+conda init
+source ~/.bashrc
+version= conda --version
+
+if $version == conda*; then
+    mkdir biobb && cd biobb
+    curl -sS https://raw.githubusercontent.com/safan41/gromacs-setup/refs/heads/main/environment.yml
+    conda env create -f environment.yml
+    echo "alias jlab=jupyter lab --NotebookApp.token='' --NotebookApp.password=''" > ~/.bashrc
+    echo "SCRIPT COMPLETE! Activate environment with 'conda activate biobb_env' and run JupyterLab with 'jlab' to continue" 
+else
+    echo 'ERROR: NO CONDA VERSION FOUND (Are you connected to the internet?)'
+fi
