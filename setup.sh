0794fb56046c789fc70daef3e3bda60416c4b1ca
Safwan Morshed <safan25641@gmail.com>
2025-07-10 14:46:57 -0500

    oops

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ source ~/.bashrc
 
 if [[ $(conda --version) = conda* ]]; then
     mkdir biobb && cd biobb
-    curl -sS https://raw.githubusercontent.com/safan41/gromacs-setup/refs/heads/main/environment.yml
+    curl https://raw.githubusercontent.com/safan41/gromacs-setup/refs/heads/main/environment.yml
     conda env create -f environment.yml
     echo "alias jlab=jupyter lab --NotebookApp.token='' --NotebookApp.password=''" > ~/.bashrc
     echo "SCRIPT COMPLETE! Activate environment with 'conda activate biobb_env' and run JupyterLab with 'jlab' to continue" 
